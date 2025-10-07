4a4e21212afa3006ff184e75232aca225bad2301
sky <159075384+skymeiling@users.noreply.github.com>
2025-10-07 02:21:03 +0000

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# Print "Hello, World!"
+
+# Create a variable holding your name. Then, print the value of the variable.
+
+# Use the input() function to make the computer ask you for your name. Then, make the computer greet you.
+
+# Fun Project: Fortune Teller!
+
+# 1) Use the input() function to ask the user if they want red, yellow, green, or blue.
+
+# 2) If they pick red, use the input() function to ask them if they want 1 or 2.
+
+# 3) If they pick yellow, use the input() function to ask them if they want 3 or 4.
+
+# 4) If they pick green, use the input() function to ask them if they want 5 or 6.
+
+# 5) If they pick blue, use the input() function to ask them if they want 7 or 8.
+
+# 6) Depending on which number they pick, print out a different fortune!
